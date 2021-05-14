9e0991ae9c1b82f9af1c1a74026cba55d7fb7124
avanhoorebeke@gmail.com <avanhoorebeke@gmail.com>
2021-05-14 17:14:20 +0200

    fix in coll

--- test/collision.c
+++ test/collision.c
@@ -1,5 +1,6 @@
 #include <check.h>
 #include "test-util.h"
+#include <stdio.h>
 
 #ifndef BLOCKS
 	#include "../src/blocks.h"
@@ -12,6 +13,7 @@
 #endif
 
 START_TEST(test_block_collision_l)
+{
 	// Make a 6x6 field,
 	// with L-block at (1,2):
 	// [ 0 0 0 0 0 0 ]
@@ -20,10 +22,11 @@ START_TEST(test_block_collision_l)
 	// [ 0 0 1 6 6 0 ]
 	// [ 0 0 1 0 0 0 ]
 	// [ 1 1 1 1 1 1 ]
-	int field[36];
+	int field[36] = {0};
 
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
+
 	field[20] = 1;
 	field[26] = 1;
 
@@ -34,11 +37,12 @@ START_TEST(test_block_collision_l)
 	// solid square, so both bottom and left should be true
 	int col = colCheck(field, 6, 6, block, 1, 2);
 
-	ck_assert_msg(col == 0b1000,
-			"BLOCK COLLISION TEST: TOUCH LEFT FAILED");
+	ck_assert(col == 0b1000);
+}
 END_TEST
 
 START_TEST(test_block_collision_r)
+{
 	// Make a 6x6 field,
 	// with L-block twice rotated at (1,1):
 	// [ 0 0 0 0 0 0 ]
@@ -47,7 +51,7 @@ START_TEST(test_block_collision_r)
 	// [ 0 6 6 1 0 0 ]
 	// [ 0 0 0 1 0 0 ]
 	// [ 1 1 1 1 1 1 ]
-	int field[36];
+	int field[36] = {0};
 
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
@@ -58,12 +62,15 @@ START_TEST(test_block_collision_r)
 	copyBlock(block, block_L);
 	// colcheck returns: 0b(left)(bottom)(right)(top) solid
 	int col = colCheck(field, 6, 6, block, 1, 0);
+	if (col != 0b0010)
+		printf("BLOCK COL R : COL ERR: COL IS %d\n", col);
 
-	ck_assert_msg(col == 0b0010,
-			"BLOCK COLLISION TEST: TOUCH RIGHT FAILED");
+	ck_assert(col == 0b0010);
+}
 END_TEST
 
 START_TEST(test_block_collision_bottom)
+{
 	// Make a 6x6 field:
 	// [ 0 0 0 0 0 0 ]
 	// [ 0 2 0 0 0 0 ]
@@ -71,7 +78,7 @@ START_TEST(test_block_collision_bottom)
 	// [ 0 2 0 1 0 0 ]
 	// [ 0 2 0 1 0 0 ]
 	// [ 1 1 1 1 1 1 ]
-	int field[36];
+	int field[36] = {0};
 
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
@@ -83,11 +90,12 @@ START_TEST(test_block_collision_bottom)
 	// colcheck returns: 0b(left)(bottom)(right)(top) solid
 	int col = colCheck(field, 6, 6, block, 1, 0);
 
-	ck_assert_msg(col == 0b0100,
-			"BLOCK COLLISION TEST: TOUCH BOTTOM FAILED");
+	ck_assert(col == 0b0100);
+}
 END_TEST
 
 START_TEST(test_block_collision_top)
+{
 	// Make a 6x6 field:
 	// [ 0 2 0 0 0 0 ]
 	// [ 0 2 0 0 0 0 ]
@@ -95,7 +103,7 @@ START_TEST(test_block_collision_top)
 	// [ 0 2 0 1 0 0 ]
 	// [ 0 0 0 1 0 0 ]
 	// [ 1 1 1 1 1 1 ]
-	int field[36];
+	int field[36] = {0};
 
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
@@ -107,11 +115,12 @@ START_TEST(test_block_collision_top)
 	// colcheck returns: 0b(left)(bottom)(right)(top) solid
 	int col = colCheck(field, 6, 6, block, 0, 0);
 
-	ck_assert_msg(col == 0b0001,
-			"BLOCK COLLISION TEST: TOUCH TOP FAILED");
+	ck_assert(col == 0b0001);
+}
 END_TEST
 
 START_TEST(test_block_collision_clipping)
+{
 	// Make a 6x6 field,
 	// with I-block, clipping
 	// at bottom:
@@ -121,7 +130,7 @@ START_TEST(test_block_collision_clipping)
 	// [ 0 2   0 1 0 0 ]
 	// [ 0 2   0 1 0 0 ]
 	// [ 1 1/2 1 1 1 1 ]
-	int field[36];
+	int field[36] = {0};
 
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
@@ -133,62 +142,58 @@ START_TEST(test_block_collision_clipping)
 	// colcheck returns: 0b(left)(bottom)(right)(top) solid
 	int col = colCheck(field, 6, 6, block, 2, 1);
 
-	ck_assert_msg(col == 0b1111,
-			"BLOCK COLLISION TEST: CLIPPING TEST FAILED");
+	ck_assert(col == 0b1111);
+}
 END_TEST
 
 START_TEST(test_block_collision_oob_left)
 {
-	int field[36];
+	int field[36] = {0};
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
 	field[27] = 1;
 	int block[4][4];
 	copyBlock(block, block_L);
 	int col = colCheck(field, 6, 6, block, 1, -2);
-	ck_assert_msg(col == 0b1111,
-			"BLOCK COLLISION TEST: OUT-OF-BOUNDS LEFT FAILED");
+	ck_assert(col == 0b1111);
 }
 END_TEST
 
 START_TEST(test_block_collision_oob_right)
 {
-	int field[36];
+	int field[36] = {0};
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
 	field[27] = 1;
 	int block[4][4];
 	copyBlock(block, block_L);
 	int col = colCheck(field, 6, 6, block, 1, 4);
-	ck_assert_msg(col == 0b1111,
-			"BLOCK COLLISION TEST: OUT-OF-BOUNDS RIGHT FAILED");
+	ck_assert(col == 0b1111);
 }
 END_TEST
 
 START_TEST(test_block_collision_oob_top)
 {
-	int field[36];
+	int field[36] = {0};
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
 	field[27] = 1;
 	int block[4][4];
 	copyBlock(block, block_L);
 	int col = colCheck(field, 6, 6, block, -1, 4);
-	ck_assert_msg(col == 0b1111,
-			"BLOCK COLLISION TEST: OUT-OF-BOUNDS TOP FAILED");
+	ck_assert(col == 0b1111);
 }
 END_TEST
 
 START_TEST(test_block_collision_oob_bottom)
 {
-	int field[36];
+	int field[36] = {0};
 	for (int i = 35; i > 29; --i)
 		field[i] = 1;
 	field[27] = 1;
 	int block[4][4];
 	copyBlock(block, block_L);
 	int col = colCheck(field, 6, 6, block, 5, 4);
-	ck_assert_msg(col == 0b1111,
-			"BLOCK COLLISION TEST: OUT-OF-BOUNDS BOTTOM FAILED");
+	ck_assert(col == 0b1111);
 }
 END_TEST
